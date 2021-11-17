1279300b4c2a5e095af7004476f05d5130c718fd
halaAloqiel <90243812+halaAloqiel@users.noreply.github.com>
2021-11-17 22:24:35 +0300

    .

--- js/main.js
+++ js/main.js
@@ -89,4 +89,55 @@ svg
         var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
         return (midangle < Math.PI ? 'start' : 'end')
 
-    })
+    })//end of pie chart
+
+
+
+  //slide on scroll animation
+  const sectionOne = document.querySelector(".animate");
+  const sliders = document.querySelectorAll(".slideIn")
+
+  const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -150px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+
+  })
